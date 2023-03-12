93461756c2e662dfdf4b9249653102d2e3183022
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-12 18:46:09 -0400

    hook up db via mongoose

--- express-backend/app.js
+++ express-backend/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Post = require('./models/post')
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+const DB_LOCAL = {
+    user: 'root',
+    password: 'example',
+    server: 'localhost',
+    port: '27017',
+    opts: 'serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256'
+}
+const DB_ENV = DB_LOCAL;
+const DATABASE_NAME = 'mean-db';
+const DATABASE_URI = `mongodb://${DB_ENV.user}:${DB_ENV.password}@${DB_ENV.server}:${DB_ENV.port}/${DATABASE_NAME}?${DB_ENV.opts}`;
+mongoose.connect(DATABASE_URI)
+    .then(() => console.log('DB connected...'))
+    .catch(err => console.error(`DB connection failed! ${err}`));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
