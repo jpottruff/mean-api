dc6c54ad3597d4a30234db90e3720ba405236513
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-12 17:14:10 -0400

    fixes CORS error

--- express-backend/app.js
+++ express-backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Mathods", "GET, POST, PATCH, DELETE, OPTIONS");
+    
+    next();
+})
+
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         {id: 1, title: 'First Post', content: "This is some content for a post"},
