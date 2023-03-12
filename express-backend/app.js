86de14295a42c038621a21c5961adf55f3ccb2e9
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-12 18:54:46 -0400

    remove STUB data and send back real data

--- express-backend/app.js
+++ express-backend/app.js
@@ -44,16 +44,16 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.use('/api/posts', (req, res, next) => {
-    const posts = [
-        {id: 1, title: 'First Post', content: "This is some content for a post"},
-        {id: 2, title: 'Second Post', content: "Its more content"},
-        {id: 3, title: 'Third Post', content: "Content of a post"},
-    ]
+    Post.find()
+        .then(documents => {
+            res.status(200).json({
+                message: 'Posts successfully fetched',
+                posts: documents
+            });
+        })
+        .catch(err => console.error(`Error getting docs: ${err}`))
     
-    res.status(200).json({
-        message: 'Posts successfully fetched',
-        posts
-    })
+
 });
 
 module.exports = app;
