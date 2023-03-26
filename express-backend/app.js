11784c99a873ee955ed38ba110a6bf1d49231b21
Jeff Pottruff <jeffpottruff@gmail.com>
2023-03-26 11:20:24 -0400

    allow authorization headers

--- express-backend/app.js
+++ express-backend/app.js
@@ -31,7 +31,7 @@ app.use('/images', express.static(path.join('express-backend/images')))
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
     
     next();
