d540656edece4cc9e435eeb41006544aac589a2c
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-09 10:05:27 -0400

    small env var mod

--- express-backend/app.js
+++ express-backend/app.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 const DATABASE_NAME = 'mean-db';
-const DATABASE_URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}/${DATABASE_NAME}?${process.env.MONGO_OPTS}`;
+const DATABASE_URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}/${DATABASE_NAME}${process.env.MONGO_OPTS}`;
 mongoose.connect(DATABASE_URI)
     .then(() => console.log('DB connected...'))
     .catch(err => console.error(`DB connection failed! ${err}`));
